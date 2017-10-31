7f115f491dd920bef7a212fb03ec17435660992d
Jordan Pittman <jordan@cryptica.me>
2017-10-31 01:17:48 -0400

    Update test names

--- test/teamcity/parser.test.js
+++ test/teamcity/parser.test.js
@@ -9,28 +9,28 @@ suite("TeamCity Parser Tests", function() {
         expect(parseLine("foo")).toBe(undefined)
     });
 
-    test("Team City lines can be parsed", function () {
+    test("lines can be parsed", function () {
         const event = parseLine("##teamcity[someEvent key='value']")
 
         expect(event.name).toBe("someEvent");
         expect(event.properties.key).toBe("value");
     });
     
-    test("Team City lines handle escaped new lines", function () {
+    test("lines handle escaped new lines", function () {
         const event = parseLine("##teamcity[someEvent key='value1|nvalue2']")
 
-        expect(event.name).toBe("someEvent", );
+        expect(event.name).toBe("someEvent");
         expect(event.properties.key).toBe("value1\nvalue2",);
     });
 
-    test("Team City lines handle escaped bars", function () {
+    test("lines handle escaped bars", function () {
         const event = parseLine("##teamcity[someEvent key='value1|nvalue2|]']")
 
-        expect(event.name).toBe("someEvent", );
+        expect(event.name).toBe("someEvent");
         expect(event.properties.key).toBe("value1\nvalue2]");
     });
 
-    test("Team City lines handle escaped quotes", function () {
+    test("lines handle escaped quotes", function () {
         const event = parseLine("##teamcity[someEvent key='value1|nvalue2|'']")
 
         expect(event.name).toBe("someEvent");
